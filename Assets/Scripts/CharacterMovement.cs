84a5d148a4bea7dbf71dac71f944fcc6508bd14f
Momchil <mk2004@live.mdx.ac.uk>
2021-11-15 10:51:53 +0000

    Push #2
    Monster Animations have been added

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -18,20 +18,29 @@ public class CharacterMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Debug.Log("mouse");
             playerChar.GetComponent<Animator>().Play("Right Top Down Attack");
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("space");
             playerChar.GetComponent<Animator>().Play("Shield");
         }
 
         if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("A");
+        { 
             playerChar.GetComponent<Animator>().Play("Dodge Left");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerChar.GetComponent<Animator>().Play("Dodge Right");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerChar.GetComponent<Animator>().Play("Block");
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            playerChar.GetComponent<Animator>().Play("Left Top Down");
         }
     }
 }
